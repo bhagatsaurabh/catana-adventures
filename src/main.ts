6bc57306fa633d628690e8d5ea52af1c52045ed6
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2024-04-08 22:04:02 +0530

    chore: scaffold

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { Boot } from "./scenes/boot";
+import { Game as MainGame } from "./scenes/game";
+import { GameOver } from "./scenes/gameover";
+import { MainMenu } from "./scenes/mainmenu";
+import { Preloader } from "./scenes/preloader";
+
+import { Game, Types } from "phaser";
+
+const config: Types.Core.GameConfig = {
+  type: Phaser.WEBGL,
+  stableSort: false,
+  width: 1024,
+  height: 768,
+  parent: "container",
+  backgroundColor: "#028af8",
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  disableContextMenu: true,
+  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
+};
+
+export default new Game(config);
